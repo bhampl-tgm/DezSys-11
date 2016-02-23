d0f599c272cb450d2dd7b1872239e056d8d8e5ab
Burkhard Hampl <bhampl@student.tgm.ac.at>
2016-02-23 11:28:59 +0100

    Added register task and added logic to view

--- app/src/main/java/at/ac/tgm/hit/dezsyslabor/hampl/dezsyslabor_11/LoginActivity.java
+++ app/src/main/java/at/ac/tgm/hit/dezsyslabor/hampl/dezsyslabor_11/LoginActivity.java
@@ -6,16 +6,33 @@ import android.annotation.TargetApi;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.SyncHttpClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 
 /**
@@ -24,22 +41,19 @@ import android.widget.EditText;
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
+     * Keep track of the login and registration task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
+    private UserRegistrationTask mRegTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
+    private EditText mNameView;
+    private Button mEmailRegisterButton;
     private View mProgressView;
     private View mLoginFormView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +61,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
-
         mPasswordView = (EditText) findViewById(R.id.password);
+        mNameView = (EditText) findViewById(R.id.name);
 
         Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
@@ -58,13 +71,45 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 attemptLogin();
             }
         });
+        mEmailRegisterButton = (Button) findViewById(R.id.email_register);
+        mEmailRegisterButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptRegister();
+            }
+        });
+
+        mTextView = (TextView) findViewById(R.id.textView);
 
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
-    }
 
-    private void populateAutoComplete() {
-        getLoaderManager().initLoader(0, null, this);
+        mNameView.addTextChangedListener(new TextWatcher() {
+
+            private void enableButtonOnText(CharSequence s) {
+                if (TextUtils.isEmpty(s)) {
+                    mEmailRegisterButton.setEnabled(false);
+                } else {
+                    mEmailRegisterButton.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // enableButtonOnText(s);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enableButtonOnText(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // enableButtonOnText(s);
+            }
+        });
+
     }
 
 
@@ -90,9 +135,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password)) {
+        if (TextUtils.isEmpty(password)) {
             mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+            focusView = mPasswordView;
             cancel = true;
         } else if (!isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
@@ -123,6 +168,68 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask.execute((Void) null);
         }
     }
+
+    private void attemptRegister() {
+        if (mRegTask != null) {
+            return;
+        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mNameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String name = mNameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(name)) {
+            mNameView.setError(getString(R.string.error_field_required));
+            focusView = mNameView;
+            cancel = true;
+        }
+
+        Log.d("attemptRegister(): ", email);
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            mRegTask = new UserRegistrationTask(email, name, password);
+            mRegTask.execute((Void) null);
+        }
+    }
+
     private boolean isEmailValid(String email) {
         //TODO: Replace this with your own logic
         return email.contains("@");
@@ -192,30 +299,60 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         private final String mEmail;
         private final String mPassword;
+        private String message;
+        private boolean fail;
 
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
+            fail = false;
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
 
+            // Login
+            SyncHttpClient client = new SyncHttpClient();
+            client.setTimeout(45000);
+            JSONObject jsonObject = new JSONObject();
+            StringEntity entity;
             try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+                jsonObject.put("email", this.mEmail);
+                jsonObject.put("password", this.mPassword);
+                entity = new StringEntity(jsonObject.toString());
+            } catch (JSONException | UnsupportedEncodingException e) {
+                e.printStackTrace();
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+            client.post(getBaseContext(), "http://dezsys-09.herokuapp.com/login", entity, RequestParams.APPLICATION_JSON, new JsonHttpResponseHandler(){
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    try {
+                        // If the response is JSONObject
+                        Log.d("HTTP POST", "SUCCESS!!");
+                        if (statusCode != 200) fail = true;
+                        message = response.getString("message");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    try {
+                        Log.d("HTTP POST", "FAILURE!! ", throwable);
+                        fail = true;
+                        if (errorResponse != null) {
+                            message = errorResponse.getString("message");
+                        } else {
+                            message = "" + R.string.message_unknown_error + statusCode;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
             // TODO: register the new account here.
             return true;
@@ -226,12 +363,14 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
 
-            if (success) {
-                finish();
+            if (success && !fail) {
+                mTextView.setTextColor(Color.BLACK);
             } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
+                mTextView.setTextColor(Color.RED);
+
             }
+            mTextView.setText(message);
+
         }
 
         @Override
@@ -240,5 +379,93 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
         }
     }
+
+    public class UserRegistrationTask extends AsyncTask<Void, Void, Boolean> {
+
+        private final String mEmail;
+        private final String mName;
+        private final String mPassword;
+        private String message;
+        private boolean fail;
+
+        UserRegistrationTask(String email, String name, String password) {
+            mEmail = email;
+            mName = name;
+            mPassword = password;
+            fail = false;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            // Login
+            SyncHttpClient client = new SyncHttpClient();
+            client.setTimeout(45000);
+            JSONObject jsonObject = new JSONObject();
+            StringEntity entity;
+            try {
+                jsonObject.put("email", this.mEmail);
+                jsonObject.put("name", this.mName);
+                jsonObject.put("password", this.mPassword);
+                entity = new StringEntity(jsonObject.toString());
+            } catch (JSONException | UnsupportedEncodingException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            client.post(getBaseContext(), "http://dezsys-09.herokuapp.com/register", entity, RequestParams.APPLICATION_JSON, new JsonHttpResponseHandler(){
+                @Override
+                public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                    try {
+                        // If the response is JSONObject
+                        Log.d("HTTP POST", "SUCCESS!!");
+                        if (statusCode != 201) fail = true;
+                        message = response.getString("message");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                    try {
+                        Log.d("HTTP POST", "FAILURE!! ", throwable);
+                        fail = true;
+                        if (errorResponse != null) {
+                            message = errorResponse.getString("message");
+                        } else {
+                            message = "" + R.string.message_unknown_error + statusCode;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mRegTask = null;
+            showProgress(false);
+
+            if (success && !fail) {
+                mTextView.setTextColor(Color.BLACK);
+            } else {
+                mTextView.setTextColor(Color.RED);
+
+            }
+            mTextView.setText(message);
+
+        }
+
+        @Override
+        protected void onCancelled() {
+            mRegTask = null;
+            showProgress(false);
+        }
+    }
+
 }
 
